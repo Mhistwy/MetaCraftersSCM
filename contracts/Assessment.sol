6fb081b9cab4921012c0b32d6801a140193c2ce4
Mhistwy <104520930+Mhistwy@users.noreply.github.com>
2024-07-12 16:47:25 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,62 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
-contract Assessment {
-    using SafeMath for uint256;
-
+contract WaveSpotSubscription {
     address payable public owner;
-    uint256 public totalBalance;
-    mapping(address => uint256) public balances;
-    mapping(address => uint256[]) public transactionHistory;
 
-    event Deposit(address indexed user, uint256 amount);
-    event Withdraw(address indexed user, uint256 amount);
+    enum SubscriptionType { Basic, Standard, Premium }
+    struct Subscriber {
+        string name;
+        SubscriptionType subscriptionType;
+    }
+
+    mapping(address => Subscriber) public subscribers;
+
+    event Subscribed(address subscriber, string name, SubscriptionType subscriptionType);
 
     constructor() {
         owner = payable(msg.sender);
     }
 
-    function deposit() external payable {
-        require(msg.value > 0, "Deposit amount must be greater than zero");
+    function subscribe(string memory _name, SubscriptionType _subscriptionType) public payable {
+        require(bytes(_name).length > 0, "Name is required");
         
-        balances[msg.sender] = balances[msg.sender].add(msg.value);
-        totalBalance = totalBalance.add(msg.value);
-        transactionHistory[msg.sender].push(msg.value);
+        uint256 subscriptionCost;
+        if (_subscriptionType == SubscriptionType.Basic) {
+            subscriptionCost = 0.01 ether;
+        } else if (_subscriptionType == SubscriptionType.Standard) {
+            subscriptionCost = 0.05 ether;
+        } else if (_subscriptionType == SubscriptionType.Premium) {
+            subscriptionCost = 0.1 ether;
+        }
+        
+        require(msg.value >= subscriptionCost, "Insufficient payment");
 
-        emit Deposit(msg.sender, msg.value);
+        subscribers[msg.sender] = Subscriber(_name, _subscriptionType);
+
+        emit Subscribed(msg.sender, _name, _subscriptionType);
     }
 
-    function withdraw(uint256 _amount) external {
-        require(_amount > 0, "Withdrawal amount must be greater than zero");
-        require(_amount <= balances[msg.sender], "Insufficient balance");
-
-        balances[msg.sender] = balances[msg.sender].sub(_amount);
-        totalBalance = totalBalance.sub(_amount);
-        transactionHistory[msg.sender].push(-int256(_amount)); // Negative amount for withdrawals
-
-        payable(msg.sender).transfer(_amount);
-
-        emit Withdraw(msg.sender, _amount);
-    }
-
-    function getBalance() external view returns (uint256) {
-        return balances[msg.sender];
-    }
-
-    function getTotalBalance() external view returns (uint256) {
-        return totalBalance;
-    }
-
-    function getTransactionHistory(address _user) external view returns (uint256[] memory) {
-        return transactionHistory[_user];
-    }
-
-    function emergencyWithdraw() external {
-        require(msg.sender == owner, "Only owner can call this function");
-
-        payable(owner).transfer(address(this).balance);
-        totalBalance = 0;
+    function getSubscription(address _subscriber) public view returns (string memory name, SubscriptionType subscriptionType) {
+        Subscriber storage subscriber = subscribers[_subscriber];
+        return (subscriber.name, subscriber.subscriptionType);
     }
 }
